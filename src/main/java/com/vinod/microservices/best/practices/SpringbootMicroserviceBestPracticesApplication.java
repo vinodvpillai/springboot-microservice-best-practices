8b9234608236d0faec1ea48e77545a01fb1e4a66
Vinod Pillai <vinodthebest@gmail.com>
2021-05-14 19:21:28 +0530

    springboot-microservice-best-practices: Added open api code
    - Added open api in the main class.

--- src/main/java/com/vinod/microservices/best/practices/SpringbootMicroserviceBestPracticesApplication.java
+++ src/main/java/com/vinod/microservices/best/practices/SpringbootMicroserviceBestPracticesApplication.java
@@ -1,8 +1,14 @@
 package com.vinod.microservices.best.practices;
 
+import com.vinod.microservices.best.practices.config.aws.LocalStackProperties;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@OpenAPIDefinition(info = @Info(title = "Spring Demo Service", description = "Spring Boot microservice best practices.", version = "1.0"))
+@EnableConfigurationProperties(LocalStackProperties.class)
 @SpringBootApplication
 public class SpringbootMicroserviceBestPracticesApplication {
 
